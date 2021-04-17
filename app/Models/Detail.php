2d321ca968415fed4004e7d0d6c00164e0845059
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-17 15:55:27 -0700

    update

--- app/Models/Detail.php
+++ app/Models/Detail.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Detail extends Model
 {
     use HasFactory;
+
+    protected $table = 'detail';
+
+    protected $guarded = [];
 }
--- app/Models/Login.php
+++ app/Models/Login.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Login extends Model
 {
     use HasFactory;
+
+    protected $table = 'login';
+
+    protected $guarded = [];
 }
