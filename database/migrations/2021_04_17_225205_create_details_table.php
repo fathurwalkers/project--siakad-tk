99ce183d70219f94f140dbf027091070da7c9fe4
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-17 15:54:15 -0700

    update

---
+++ database/migrations/2021_04_17_225205_create_details_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('detail', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('detail');
+    }
+}
