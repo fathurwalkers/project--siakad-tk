cce8c42b8035d512e1bebb7b993e0e7105543f6a
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-17 15:57:57 -0700

    update

---
+++ app/Http/Controllers/Admin/BackController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class BackController extends Controller
+{
+    public function index()
+    {
+        return "Index Page";
+    }
+}
