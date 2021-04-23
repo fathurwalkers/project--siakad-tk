4c7003f5aba3a35367f3100e1fe38eced5a0279f
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-23 14:53:44 -0700

    update

---
+++ resources/views/components/admin-navbar.blade.php
@@ -0,0 +1,95 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library -->
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <p>
+            Dashboard
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="{{ asset('vendor/adminlte/') }}/index.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Dashboard v1</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ asset('vendor/adminlte/') }}/index2.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Dashboard v2</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ asset('vendor/adminlte/') }}/index3.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Dashboard v3</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-copy"></i>
+          <p>
+            Layout Options
+            <i class="fas fa-angle-left right"></i>
+            <span class="badge badge-info right">6</span>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="../layout/top-nav.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Top Navigation</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/top-nav-sidebar.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Top Navigation + Sidebar</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/boxed.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Boxed</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/fixed-sidebar.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Fixed Sidebar</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/fixed-sidebar-custom.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Fixed Sidebar <small>+ Custom Area</small></p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/fixed-topnav.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Fixed Navbar</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/fixed-footer.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Fixed Footer</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/collapsed-sidebar.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Collapsed Sidebar</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
