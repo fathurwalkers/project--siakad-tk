a31255bad600d2e95b3bb0603f4d90769b77c6e8
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-24 08:27:31 -0700

    update

--- resources/views/admin/user/daftar-user.blade.php
+++ resources/views/admin/user/daftar-user.blade.php
@@ -13,83 +13,58 @@
         
             <div class="card">
                 <div class="card-header">
-                  <h3 class="card-title">DataTable with default features</h3>
+                    {{-- <div class="row"> --}}
+                            <div class="col-lg-12 col-sm-12 col-md-12">
+                                <h3 class="card-title d-flex my-auto py-auto">Daftar User</h3>
+                            </div>
+                    {{-- </div> --}}
                 </div>
+
                 <!-- /.card-header -->
                 <div class="card-body">
-                  <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4"><div class="row"><div class="col-sm-12 col-md-6"><div class="dataTables_length" id="example1_length"><label>Show <select name="example1_length" aria-controls="example1" class="custom-select custom-select-sm form-control form-control-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-12 col-md-6"><div id="example1_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="example1" class="table table-bordered table-striped dataTable" role="grid" aria-describedby="example1_info">
-                    <thead>
-                    <tr role="row"><th class="sorting_asc" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Rendering engine: activate to sort column descending" style="width: 170px;">Rendering engine</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Browser: activate to sort column ascending" style="width: 219px;">Browser</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Platform(s): activate to sort column ascending" style="width: 194px;">Platform(s)</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="Engine version: activate to sort column ascending" style="width: 143px;">Engine version</th><th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1" aria-label="CSS grade: activate to sort column ascending" style="width: 101px;">CSS grade</th></tr>
-                    </thead>
-                    <tbody>
-                    <tr role="row" class="odd">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Firefox 1.0</td>
-                      <td>Win 98+ / OSX.2+</td>
-                      <td>1.7</td>
-                      <td>A</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Firefox 1.5</td>
-                      <td>Win 98+ / OSX.2+</td>
-                      <td>1.8</td>
-                      <td>A</td>
-                    </tr><tr role="row" class="odd">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Firefox 2.0</td>
-                      <td>Win 98+ / OSX.2+</td>
-                      <td>1.8</td>
-                      <td>A</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Firefox 3.0</td>
-                      <td>Win 2k+ / OSX.3+</td>
-                      <td>1.9</td>
-                      <td>A</td>
-                    </tr><tr role="row" class="odd">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Camino 1.0</td>
-                      <td>OSX.2+</td>
-                      <td>1.8</td>
-                      <td>A</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Camino 1.5</td>
-                      <td>OSX.3+</td>
-                      <td>1.8</td>
-                      <td>A</td>
-                    </tr><tr role="row" class="odd">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Netscape 7.2</td>
-                      <td>Win 95+ / Mac OS 8.6-9.2</td>
-                      <td>1.7</td>
-                      <td>A</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Netscape Browser 8</td>
-                      <td>Win 98SE+</td>
-                      <td>1.7</td>
-                      <td>A</td>
-                    </tr><tr role="row" class="odd">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Netscape Navigator 9</td>
-                      <td>Win 98+ / OSX.2+</td>
-                      <td>1.8</td>
-                      <td>A</td>
-                    </tr><tr role="row" class="even">
-                      <td class="sorting_1">Gecko</td>
-                      <td>Mozilla 1.0</td>
-                      <td>Win 95+ / OSX.1+</td>
-                      <td>1</td>
-                      <td>A</td>
-                    </tr></tbody>
-                    <tfoot>
-                    <tr><th rowspan="1" colspan="1">Rendering engine</th><th rowspan="1" colspan="1">Browser</th><th rowspan="1" colspan="1">Platform(s)</th><th rowspan="1" colspan="1">Engine version</th><th rowspan="1" colspan="1">CSS grade</th></tr>
-                    </tfoot>
-                  </table></div></div><div class="row"><div class="col-sm-12 col-md-5"><div class="dataTables_info" id="example1_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-12 col-md-7"><div class="dataTables_paginate paging_simple_numbers" id="example1_paginate"><ul class="pagination"><li class="paginate_button page-item previous disabled" id="example1_previous"><a href="#" aria-controls="example1" data-dt-idx="0" tabindex="0" class="page-link">Previous</a></li><li class="paginate_button page-item active"><a href="#" aria-controls="example1" data-dt-idx="1" tabindex="0" class="page-link">1</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="2" tabindex="0" class="page-link">2</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="3" tabindex="0" class="page-link">3</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="4" tabindex="0" class="page-link">4</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="5" tabindex="0" class="page-link">5</a></li><li class="paginate_button page-item "><a href="#" aria-controls="example1" data-dt-idx="6" tabindex="0" class="page-link">6</a></li><li class="paginate_button page-item next" id="example1_next"><a href="#" aria-controls="example1" data-dt-idx="7" tabindex="0" class="page-link">Next</a></li></ul></div></div></div></div>
+
+                    <button class="btn btn-info d-flex my-auto py-auto mr-3 float-right" onclick="window.location.href='{{ route('admin-index') }}'">Kembali</button>
+
+                    <table id="example" class="table table-bordered table-hover dataTable" style="width:100%">
+                        <thead class="bg-dark text-white shadow">
+                            <tr>
+                                <th class="text-center">#</th>
+                                <th>Nama</th>
+                                <th>Telepon</th>
+                                <th>Alamat</th>
+                                <th>Gender</th>
+                                <th>Role</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ( $detail as $item )
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>{{ $item->detail_nama }}</td>
+                                    <td>{{ $item->detail_telepon }}</td>
+                                    <td>{{ $item->detail_alamat }}</td>
+                                    <td>
+                                        @if ($item->detail_jeniskelamin == 'L')
+                                        Laki - Laki 
+                                        @elseif ($item->detail_jeniskelamin == 'P')
+                                        Perempuan
+                                        @endif   
+                                    </td>
+                                    <td>
+                                        @if ($item->detail_role == 'guest')
+                                        <span class="badge badge-info">USER</span>
+                                        @elseif ($item->detail_role == 'user')
+                                        <span class="badge badge-success">GUEST</span>
+                                        @endif       
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
                 </div>
                 <!-- /.card-body -->
-              </div>
+            </div>
 
         </div>
 
@@ -100,16 +75,8 @@
 <script src="{{ asset('vendor/adminlte/') }}/plugins/datatables/jquery.dataTables.js"></script>
 <script src="{{ asset('vendor/adminlte/') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
 <script>
-  $(function () {
-    $("#example1").DataTable();
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
+    $(document).ready(function() {
+        $('#example').DataTable();
     });
-  });
 </script>
 @endpush
