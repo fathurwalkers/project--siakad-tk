64c66c5015573dab62586f486bd0525a0684e905
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-24 05:01:43 -0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,48 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Login;
+use App\Models\Detail;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $jeniskelamin               = 'L';
+        $detail                     = new Detail;
+        $login                      = new Login;
+        $saveDetail = $detail->create([
+            'detail_nama'           => 'Administrator',
+            'detail_telepon'        => '085494939129',
+            'detail_alamat'         => 'No Address',
+            'detail_jeniskelamin'   => $jeniskelamin,
+            'created_at'            => now(),
+            'updated_at'            => now()
+        ]);
+        $saveDetail->save();
+        
+
+        $username               = 'admin';
+        $password               = Hash::make($username, [
+                                        'rounds' => 12,
+                                    ]);
+        $email                  = strtolower($username);
+        $email                 .= '@siakadtk.com';
+        $level                  = 'admin';
+        $status                 = 'verified';
+
+        $token = Str::random(16);
+        $saveLogin = $login->create([
+            'login_username'    => $username,
+            'login_password'    => $password,
+            'login_email'       => $email,
+            'login_token'       => $token,
+            'login_level'       => $level,
+            'login_status'      => $status,
+            'created_at'        => now(),
+            'updated_at'        => now(),
+        ]);
+        $saveLogin->detail()->associate($saveDetail->id);
+        $saveLogin->save();
     }
 }
