5269e2400d1c5def0bf846cdc95c1caba856aef5
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-24 04:54:31 -0700

    update

--- resources/views/components/admin-navbar.blade.php
+++ resources/views/components/admin-navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-      
+
       <li class="nav-item">
         <a href="#" class="nav-link">
           <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -35,9 +35,84 @@
         <a href="#" class="nav-link">
           <i class="nav-icon fas fa-copy"></i>
           <p>
+            Post
+            <i class="fas fa-angle-left right"></i>
+            {{-- <span class="badge badge-info right">6</span> --}}
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="../layout/top-nav.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Top Navigation</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/boxed.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Boxed</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-copy"></i>
+          <p>
             Kelas
             <i class="fas fa-angle-left right"></i>
-            <span class="badge badge-info right">6</span>
+            {{-- <span class="badge badge-info right">6</span> --}}
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="../layout/top-nav.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Top Navigation</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/boxed.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Boxed</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-copy"></i>
+          <p>
+            Guru
+            <i class="fas fa-angle-left right"></i>
+            {{-- <span class="badge badge-info right">6</span> --}}
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="../layout/top-nav.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Top Navigation</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="../layout/boxed.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Boxed</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-copy"></i>
+          <p>
+            Siswa
+            <i class="fas fa-angle-left right"></i>
+            {{-- <span class="badge badge-info right">6</span> --}}
           </p>
         </a>
         <ul class="nav nav-treeview">
