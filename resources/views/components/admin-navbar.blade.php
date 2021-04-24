fd0d9dde04b4fd596cfc5573c518f9da3aa1431d
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-24 04:52:50 -0700

    update

--- resources/views/components/admin-navbar.blade.php
+++ resources/views/components/admin-navbar.blade.php
@@ -1,7 +1,6 @@
 <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-      <!-- Add icons to the links using the .nav-icon class
-           with font-awesome or any other icon font library -->
+      
       <li class="nav-item">
         <a href="#" class="nav-link">
           <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -14,28 +13,29 @@
           <li class="nav-item">
             <a href="{{ asset('vendor/adminlte/') }}/index.html" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
-              <p>Dashboard v1</p>
+              <p>Lihat Profil</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="{{ asset('vendor/adminlte/') }}/index2.html" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
-              <p>Dashboard v2</p>
+              <p>Edit Profil</p>
             </a>
           </li>
           <li class="nav-item">
             <a href="{{ asset('vendor/adminlte/') }}/index3.html" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
-              <p>Dashboard v3</p>
+              <p>Tambah User</p>
             </a>
           </li>
         </ul>
       </li>
+
       <li class="nav-item">
         <a href="#" class="nav-link">
           <i class="nav-icon fas fa-copy"></i>
           <p>
-            Layout Options
+            Kelas
             <i class="fas fa-angle-left right"></i>
             <span class="badge badge-info right">6</span>
           </p>
@@ -48,48 +48,13 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="../layout/top-nav-sidebar.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Top Navigation + Sidebar</p>
-            </a>
-          </li>
-          <li class="nav-item">
             <a href="../layout/boxed.html" class="nav-link">
               <i class="far fa-circle nav-icon"></i>
               <p>Boxed</p>
             </a>
           </li>
-          <li class="nav-item">
-            <a href="../layout/fixed-sidebar.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Fixed Sidebar</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="../layout/fixed-sidebar-custom.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Fixed Sidebar <small>+ Custom Area</small></p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="../layout/fixed-topnav.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Fixed Navbar</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="../layout/fixed-footer.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Fixed Footer</p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="../layout/collapsed-sidebar.html" class="nav-link">
-              <i class="far fa-circle nav-icon"></i>
-              <p>Collapsed Sidebar</p>
-            </a>
-          </li>
         </ul>
       </li>
+
     </ul>
   </nav>
