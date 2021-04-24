832f92f1ab47bf70fb27bcc44f1f143c50cd00c2
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-24 07:55:01 -0700

    update

--- resources/views/admin/user/daftar-user.blade.php
+++ resources/views/admin/user/daftar-user.blade.php
@@ -99,4 +99,17 @@
 @push('after-script')
 <script src="{{ asset('vendor/adminlte/') }}/plugins/datatables/jquery.dataTables.js"></script>
 <script src="{{ asset('vendor/adminlte/') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<script>
+  $(function () {
+    $("#example1").DataTable();
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+    });
+  });
+</script>
 @endpush
