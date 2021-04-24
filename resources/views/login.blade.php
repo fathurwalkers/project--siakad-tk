7ef8d471ccd1a87ccefae48c9dabc814e88ca639
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-24 06:53:43 -0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,8 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in (v2)</title>
-
+  <title>Login Page</title>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
@@ -22,8 +21,6 @@
       <a href="#" class="h1"><b>LOGIN</b></a>
     </div>
     <div class="card-body">
-
-        {{-- <p class="login-box-msg">Sign in to start your session</p> --}}
 
         @if (session('login_fail'))
             <div class="alert alert-danger">
