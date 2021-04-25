790b1cfa4f49c574f7dd284086bcf70af4268372
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-25 10:36:41 -0700

    update

--- app/Http/Controllers/Admin/BackController.php
+++ app/Http/Controllers/Admin/BackController.php
@@ -71,7 +71,6 @@ class BackController extends Controller
     public function postLogin(Request $request)
     {
         $cariUser = Login::where('login_username', $request->login_username)->get();
-        // dd($cariUser);
         if ($cariUser->isEmpty()) {
             return back()->with('login_fail', 'Maaf, username atau password salah!')->withInput();
         } else {
