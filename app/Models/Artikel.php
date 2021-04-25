472ac3fc868c954333d1ba8b3d0c1eac74ece3f8
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-25 11:08:09 -0700

    update

--- app/Models/Artikel.php
+++ app/Models/Artikel.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Kategori;
+use App\Models\Login;
+use App\Models\Gambar;
 
 class Artikel extends Model
 {
     use HasFactory;
+
+    protected $table = 'artikel';
+    
+    protected $guarded = [];
+
+    public function kategori()
+    {
+        return $this->belongsTo(Kategori::class);
+    }
+
+    public function login()
+    {
+        return $this->belongsTo(Login::class);
+    }
+
+    public function gambar()
+    {
+        return $this->belongsTo(Gambar::class);
+    }
 }
--- database/migrations/2021_04_25_180649_create_artikels_table.php
+++ database/migrations/2021_04_25_180649_create_artikels_table.php
@@ -6,26 +6,31 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateArtikelsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::create('artikels', function (Blueprint $table) {
+        Schema::create('artikel', function (Blueprint $table) {
             $table->id();
+            $table->text('artikel_judul');
+            $table->text('artikel_body');
+            $table->text('artikel_slug');
+            $table->text('artikel_status');
+            $table->text('artikel_penulis');
+
+            $table->unsignedBigInteger('gambar_id')->default(null)->nullable();
+            $table->foreign('gambar_id')->references('id')->on('gambar')->onDelete('cascade');
+
+            $table->unsignedBigInteger('login_id')->default(null)->nullable();
+            $table->foreign('login_id')->references('id')->on('login')->onDelete('cascade');
+
+            $table->unsignedBigInteger('kategori_id')->default(null)->nullable();
+            $table->foreign('kategori_id')->references('id')->on('kategori')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('artikels');
+        Schema::dropIfExists('artikel');
     }
 }
