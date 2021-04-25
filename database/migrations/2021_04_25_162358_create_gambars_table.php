33c226eaacb28d3647d271896066beee79107adf
Fathur Walkers <fathurwalkers44@gmail.com>
2021-04-25 09:24:38 -0700

    update

---
+++ database/migrations/2021_04_25_162358_create_gambars_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGambarsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('gambar', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('gambar');
+    }
+}
