1f02d271c197dfe6701d33d0f1e7c4a812bb1545
Fathur Walkers <fathurwalkers44@gmail.com>
2021-05-16 21:50:54 -0700

    update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -96,7 +96,7 @@
                 </div>
                 <!-- /.col -->
                 <div class="col-4">
-                  <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                  <button type="submit" class="btn btn-primary btn-block">REGISTER</button>
                 </div>
                 <!-- /.col -->
             </div>
